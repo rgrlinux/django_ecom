ef789b678b9c18443533bc9d06f0d7346a3413a5
rgrlinux <rgrlinux@gmail.com>
2020-06-06 17:44:01 -0300

    add contact pages and about

--- oz_e_commerce/urls.py
+++ oz_e_commerce/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import home_page
+from .views import home_page, contact_page, about_page
 
 
 urlpatterns = [
     path('', home_page),
+    path('about/', about_page),
+    path('contact/', contact_page),
     path('admin/', admin.site.urls),
 ]
--- oz_e_commerce/views.py
+++ oz_e_commerce/views.py
@@ -3,4 +3,24 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'index.html', {})
+    context = {
+        'title': 'Oz42-Ecomm',
+        'content': 'Bem vindo a Oz42'
+    }
+    return render(request, 'index.html', context)
+
+
+def about_page(request):
+    context = {
+        'title': 'Sobre',
+        'content': 'Bem vindo a Oz42'
+    }
+    return render(request, 'about/view.html', context)
+
+
+def contact_page(request):
+    context = {
+        'title': 'Página de contato',
+        'content': 'Bem-vindo a página de contato'
+    }
+    return render(request, 'contact/view.html', context)
